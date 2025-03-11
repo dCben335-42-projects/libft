a5bfe91810d9d24d32eae2f29c265bc919385269
dCben335 <benoit.cabocel.335@gmail.com>
2025-03-11 20:41:10 +0100

    Add BASE var in types.h

--- includes/types.h
+++ includes/types.h
@@ -6,7 +6,7 @@
 /*   By: bcabocel <bcabocel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:59:48 by bcabocel          #+#    #+#             */
-/*   Updated: 2025/03/11 20:10:15 by bcabocel         ###   ########.fr       */
+/*   Updated: 2025/03/11 20:38:56 by bcabocel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,12 @@
 # define ULONG_MAX 18446744073709551615
 # define ULONG_MAX_STR "18446744073709551615"
 # define ULONG_MAX_LEN 20
+
+# define BASE_2 "01"
+# define BASE_8 "01234567"
+# define BASE_10 "0123456789"
+# define BASE_16 "0123456789abcdef"
+# define BASE_16_UPPER "0123456789ABCDEF"
 
 typedef enum e_bool
 {
